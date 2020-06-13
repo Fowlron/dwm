2432fc15fa442c02cf59a3c96a35aad095d65dec
Fowlron <pedromfgodinho@gmail.com>
2020-06-13 17:55:40 +0100

    add viewontag

--- config.def.h
+++ config.def.h
@@ -18,6 +18,8 @@ static const char *colors[][3]      = {
 	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
 };
 
+static const Bool viewontag         = True;
+
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
