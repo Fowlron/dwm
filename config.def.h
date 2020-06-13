9165c6fa6d724cc53760e283f4bb0325b73e426d
Fowlron <pedromfgodinho@gmail.com>
2020-06-13 21:10:27 +0100

    reverted

--- config.def.h
+++ config.def.h
@@ -8,7 +8,7 @@ static const unsigned int gappx     = 6;
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 
-static const char *fonts[]          = { "MesloLGS NF Regular:size=11", "JoyPixels:size=11"};
+static const char *fonts[]          = { "MesloLGS NF Regular:size=11" };
 static const char dmenufont[]       = "monospace:size=10";
 
 /* Colors */
