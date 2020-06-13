2f090f53b51c14f1f1e375fe6d139e2634216358
Fowlron <pedromfgodinho@gmail.com>
2020-06-13 17:45:51 +0100

    removed selfrestart

--- config.def.h
+++ config.def.h
@@ -59,7 +59,6 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "/home/pedro/.config/kitty/start_shell.sh", NULL };
 
-#include "selfrestart.c"
 #include "movestack.c"
 static Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -107,7 +106,6 @@ static Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
-	{ MODKEY|ShiftMask,             XK_r,      self_restart,   {0} },
 };
 
 /* button definitions */
