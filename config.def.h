642f4b43b696a704ddd4f7a9c337a61f2ba77d71
Fowlron <pedromfgodinho@gmail.com>
2020-06-13 19:08:10 +0100

    typo

--- config.def.h
+++ config.def.h
@@ -23,7 +23,7 @@ static const char *const autostart[] = {
     "picom", NULL, 
     "/usr/lib/mate-polkit/polkit-mate-authentication-agent-1", NULL,
     "bash", "~/.wallpaper/wallpaper_timer", NULL,
-    "~/xbindkeys", NULL,
+    "xbindkeys", NULL,
     NULL
 };
 
